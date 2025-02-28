a9a567fb6de5fa653d52414e177678a8ffb864ea
nytewalker <zubbiesmart@gmail.com>
2025-02-28 13:25:08 +0100

    created a declaration file for typescript

---
+++ frontend/src/declaration.d.ts
@@ -0,0 +1,15 @@
+// src/declaration.d.ts  
+declare module '*.jpg' {  
+    const src: string;  
+    export default src;  
+  }  
+  
+  declare module '*.jpeg' {  
+    const src: string;  
+    export default src;  
+  }  
+  
+  declare module '*.png' {  
+    const src: string;  
+    export default src;  
+  }  
